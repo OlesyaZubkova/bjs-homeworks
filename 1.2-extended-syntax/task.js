fbe78b87b619aae90af3831bfc52d00a52789e88
Olesya Zubkova <olesya_2802@mail.ru>
2020-11-12 09:55:32 +0300

    task updated

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,54 +1,39 @@
+"use strict";
+
 function getResult(a, b, c) {
-    "use strict";
 
     let discriminant = (Math.pow(b, 2) - 4 * a * c);
     let array = [];
-
-    if (discriminant < 0) {
-        array;
-    } else if (discriminant === 0) {
-        let oneRoot = (-b / (2 * a));
-        array.push(oneRoot);
+    
+    if (discriminant === 0) {
+        array.push(-b / (2 * a));
     } else if (discriminant > 0) {
-        let firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
-        array.push(firstRoot);
-        let secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
-        array.push(secondRoot);
+        array.push((-b + Math.sqrt(discriminant)) / (2 * a));
+        array.push((-b - Math.sqrt(discriminant)) / (2 * a));
     }
-
-    let result = array;
-    return result;
+    return array;
 }
 
 function getAverageMark(marks) {
-    marks.splice(5);
     
     if (marks.length === 0) {
       return 0;
     } else if (marks.length > 5) {
       console.log("Количество оценок больше 5!");
-    } else {
+      marks.splice(5);
+    } 
       let sum = 0;
-      let count = marks.length;
-      for (let i = 0; i < count; i++) {
+      for (let i = 0; i < marks.length; i++) {
         sum += marks[i]
-      };
-      return sum / count;
     }
-    let averageMark = sum / count;
-    return averageMark;
+    return sum / marks.length;
 }
 
 function askDrink(name, dateOfBirthday) {
 
-    dateOfBirthday = new Date(dateOfBirthday);
-    let now = new Date();
-    let age = now.getFullYear() - dateOfBirthday.getFullYear();
-    let message;
-    if (age >= 18) {
-    message = (`Не желаете ли олд-фэшн, ${name}?`);
-  } else if (age < 18) {
-    message = (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+    if (new Date().getFullYear() - dateOfBirthday.getFullYear() >= 18) {
+    return (`Не желаете ли олд-фэшн, ${name}?`);
+  } else {
+    return (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
   }
-  return message;
 }
