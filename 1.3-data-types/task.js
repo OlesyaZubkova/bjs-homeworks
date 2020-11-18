9dc4d036dd84b6ab606fcc4d0cefe1271c60aa9f
Olesya Zubkova <olesya_2802@mail.ru>
2020-11-18 16:22:46 +0300

    task changed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    if (isNaN(percent)) {
+        return (`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
+    };
+
+    if (isNaN(contribution)) {
+        return (`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`);
+    };
+
+    if (isNaN(amount)) {
+        return (`Параметр "Общая стоимость" содержит неправильное значение ${amount}`);
+    };
+    
+
+    let S = amount - contribution;
+    let p = percent / 1200;
+    let n = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date ().getMonth());
+
+    let monthPayment = S * (p + p / ((Math.pow((1 + p), n)) - 1));
+    let payment = monthPayment * n;
+
+    return Number(payment.toFixed(2)); 
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name) {
+        return  (`Привет, мир! Меня зовут ${name}.`);
+    } else {
+        return (`Привет, мир! Меня зовут Аноним.`);
+    }
 }
