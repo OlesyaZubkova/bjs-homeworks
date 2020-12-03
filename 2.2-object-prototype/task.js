c0bc23e5b7f304dd592ec1db5ece231644a65b51
Olesya Zubkova <olesya_2802@mail.ru>
2020-12-03 17:17:12 +0300

    task changed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,43 @@
-//String.prototype.isPalindrome - для задачи №1
+// task 1
+
+String.prototype.isPalindrome = function isPalindrome() {
+
+    const usualString = this.toLowerCase().split(" ").join("");
+    const reverseString = this.toLowerCase().split("").reverse().join("").replace(/[ ,\.]/g,'');
+
+    if (usualString === reverseString) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+// task 2
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+    };
+
+    const roundedAverage = Math.round(sum / marks.length);
+
+    if (sum === 0) {
+      return 0;
+    } else {
+      return roundedAverage;
+    };
+};
+
+// task 3
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+
+    const now = Date.now();
+    birthday = new Date(birthday).getTime();
+    const diff = now - birthday;
+    const age = diff / (24 * 3600 * 365.25 * 1000);
+
+    return age > 18;
+};
